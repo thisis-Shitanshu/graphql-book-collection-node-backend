24c77d9058cf5a6d0116051b53a1d32f91833b09
namaste-code <shitanshu.19p@gmail.com>
2019-05-08 02:09:41 +0530

    Creating Root Query.

--- schema/schema.js
+++ schema/schema.js
@@ -1,8 +1,15 @@
 const graphql = require('graphql');
 
+/**
+ * Define Types.
+ * Relationship between types.
+ * Defining root-queries.
+ */
+
 const { 
     GraphQLObjectType,
-    GraphQLString
+    GraphQLString,
+    GraphQLSchema
 } = graphql;
 
 const BookType = new GraphQLObjectType({
@@ -12,4 +19,25 @@ const BookType = new GraphQLObjectType({
         name: { type: GraphQLString },
         genere: { type: GraphQLString }
     })
+});
+
+/**
+ * Each root query will be a field on it's own.
+ */
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: { id: { type: GraphQLString } },
+            resolve(parent, args) {
+                // code to get data from db / other source.
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 });
