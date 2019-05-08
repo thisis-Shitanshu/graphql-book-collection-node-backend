a86a34883ad06bd6f2137b3de462b2d81247897d
namaste-code <shitanshu.19p@gmail.com>
2019-05-08 13:15:58 +0530

    GraphQL Lists

--- schema/schema.js
+++ schema/schema.js
@@ -12,14 +12,18 @@ const {
     GraphQLString,
     GraphQLSchema,
     GraphQLID,
-    GraphQLInt
+    GraphQLInt,
+    GraphQLList
 } = graphql;
 
 // dummy data
 var books = [
     {name: 'Name of the Wind', genere: 'Fantasy', id: '1', authorId: '1'},
     {name: 'The final Empire', genere: 'Fantasy', id: '2', authorId: '2'},
-    {name: 'The Long Earth', genere: 'Sci-Fi', id: '3', authorId: '3'}
+    {name: 'The Long Earth', genere: 'Sci-Fi', id: '3', authorId: '3'},
+    {name: 'THe Hero of Ages', genere: 'Fantasy', id: '4', authorId: '3'},
+    {name: 'The Color of Magic', genere: 'Fantasy', id: '5', authorId: '3'},
+    {name: 'The Light Fantastic', genere: 'Fantasy', id: '6', authorId: '3'}
 ];
 
 var authors = [
@@ -27,6 +31,10 @@ var authors = [
     {name: 'Brandon Sanderson', age: 42, id: '2'},
     {name: 'Terry Pratchett', age: 66, id: '3'}
 ];
+
+/**
+ * The reason to define fiels in function is not to execute them unless called for.
+ */
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -37,7 +45,7 @@ const BookType = new GraphQLObjectType({
         author: {
             type: AuthorType,
             resolve(parent, args) {
-                 return _.find(authors, { id: parent.authorId })
+                 return _.find(authors, { id: parent.authorId });
             }
         }
     })
@@ -48,7 +56,13 @@ const AuthorType = new GraphQLObjectType({
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
-        age: { type: GraphQLInt }
+        age: { type: GraphQLInt },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args) {
+                return _.filter(books, { authorId: parent.id });
+            }
+        }
     })
 });
 
