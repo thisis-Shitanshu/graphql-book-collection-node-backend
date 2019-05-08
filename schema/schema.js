e82be64ea0454b312dd7cd49094391b7a1b17fb3
namaste-code <shitanshu.19p@gmail.com>
2019-05-08 15:46:46 +0530

    Dealing with non-null mutations.

--- schema/schema.js
+++ schema/schema.js
@@ -16,7 +16,8 @@ const {
     GraphQLSchema,
     GraphQLID,
     GraphQLInt,
-    GraphQLList
+    GraphQLList,
+    GraphQLNonNull
 } = graphql;
 
 
@@ -104,8 +105,8 @@ const Mutation = new GraphQLObjectType({
         addAuthor:{
             type: AuthorType,
             args: {
-                name: { type: GraphQLString },
-                age:{ type: GraphQLInt }
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                age:{ type: new GraphQLNonNull(GraphQLInt) }
             },
             resolve(parent, args) {
                 let author = new Author({
@@ -118,9 +119,9 @@ const Mutation = new GraphQLObjectType({
         addBook: {
             type: BookType,
             args: {
-                name: { type: GraphQLString },
-                genere: { type: GraphQLString },
-                authorId: { type: GraphQLID }
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                genere: { type: new GraphQLNonNull(GraphQLString) },
+                authorId: { type: new GraphQLNonNull(GraphQLID) }
             },
             resolve(parent, args) {
                 let book = new Book({
