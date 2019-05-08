f0156439fb6546b86dcc99676a45b5faf11c29fc
namaste-code <shitanshu.19p@gmail.com>
2019-05-08 14:15:22 +0530

    Connecting to MongoDB

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
 
 const schema = require('./schema/schema');
+const devkeys = require('./dev');
 
 const app = express();
+
+// Connect to MongoDB Atlas
+mongoose.connect(`mongodb+srv://shaun:${devkeys["mongodb-atlas-password"]}@cluster0-l88bj.mongodb.net/test?retryWrites=true`);
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
